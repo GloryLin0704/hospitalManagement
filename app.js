284bb848a0febcc93fc8c74972a7ea740dcd8e1c
RY lin <1019022357@qq.com>
2019-01-15 22:34:39 +0800

    其他配置

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const cors = require("koa2-cors");
+
 
 const api = require('./routes/api')
 
@@ -15,6 +17,8 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
+
+app.use(cors());
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
